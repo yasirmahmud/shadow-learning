1ec3637f0035d31f2ba319ea13442eaa7b2072ba
yasirmahmud <ymahmud@ufl.edu>
2025-10-24 20:13:08 -0400

    second

--- assets/ui.js
+++ assets/ui.js
@@ -1,2 +1,15 @@
-export const qs=(s,r=document)=>r.querySelector(s);
-export const el=(tag,attrs={},...children)=>{const n=document.createElement(tag);Object.entries(attrs).forEach(([k,v])=>{if(k==='class')n.className=v;else n.setAttribute(k,v)});children.forEach(c=>n.append(c.nodeType?c:document.createTextNode(c)));return n;};
+export const qs = (s, r=document) => r.querySelector(s);
+export const el = (tag, attrs={}, ...children) => {
+  const node = document.createElement(tag);
+  Object.entries(attrs).forEach(([k,v]) => node.setAttribute(k, v));
+  children.forEach(c => node.append(c.nodeType ? c : document.createTextNode(c)));
+  return node;
+};
+export const toBlobDownload = (text, filename, type='text/plain') => {
+  const blob = new Blob([text], { type });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url; a.download = filename;
+  a.click();
+  setTimeout(() => URL.revokeObjectURL(url), 500);
+};
