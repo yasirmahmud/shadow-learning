98d8d2391f870aa37d2a67d8f159006c0344bac2
yasirmahmud <ymahmud@ufl.edu>
2025-10-24 20:29:40 -0400

    no pop up

--- assets/ui.js
+++ assets/ui.js
@@ -1,5 +1,4 @@
 export const qs = (s, r=document) => r.querySelector(s);
-export const qsp = (s, r=document) => [...r.querySelectorAll(s)];
 export const el = (tag, attrs={}, ...children) => {
   const node = document.createElement(tag);
   Object.entries(attrs).forEach(([k,v]) => {
@@ -13,8 +12,7 @@ export const el = (tag, attrs={}, ...children) => {
 export const toBlobDownload = (text, filename, type='text/plain') => {
   const blob = new Blob([text], { type });
   const url = URL.createObjectURL(blob);
-  const a = document.createElement('a');
-  a.href = url; a.download = filename; a.click();
+  const a = document.createElement('a'); a.href = url; a.download = filename; a.click();
   setTimeout(() => URL.revokeObjectURL(url), 500);
 };
 export const storageKey = (courseId) => `answers:${courseId}`;
